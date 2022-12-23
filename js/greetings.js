82048d97185c27cf7de5d83176eba845bd561fd9
Eunbeen Oh <svbean77@naver.com>
2022-12-23 16:53:01 +0900

    edit some id and location

--- js/greetings.js
+++ js/greetings.js
@@ -1,7 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const resetForm = document.querySelector("#logout-form");
+const resetForm = document.querySelector("#reset-form");
 
 console.log(resetForm);
 
